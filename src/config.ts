4e2861bccdf519e1b7a7350cb554727bd6e4661f
Vladimir Rujević <vladimir.rujevic@live.com>
2025-08-07 21:37:50 +0200

    Add a configuration management system with read and write support

---
+++ src/config.ts
@@ -0,0 +1,51 @@
+import fs from "fs";
+import os from "os";
+import path from "path";
+
+const getConfigFilePath = ():string => {
+    return path.join(os.homedir(), ".gatorconfig.json");
+}
+
+const writeConfig = (config: Config) => {
+    const configFilePath = getConfigFilePath();
+
+    const rawConfig = {
+        db_url: config.dbUrl,
+        current_user_name: config.currentUserName,
+    }
+
+    const stringifiedConfig = JSON.stringify(rawConfig);
+    fs.writeFileSync(configFilePath, stringifiedConfig);
+}
+
+const validateConfig = (rawConfig: any): Config => {
+
+    if (!rawConfig.db_url || typeof rawConfig.db_url !== "string") {
+        throw new Error("db_url is required in config file");
+    }
+    if (!rawConfig.current_user_name || typeof rawConfig.current_user_name !== "string") {
+        throw new Error("current_user_name is required in config file");
+    }
+    return {
+        dbUrl: rawConfig.db_url,
+        currentUserName: rawConfig.current_user_name,
+    } as Config;
+}
+
+export function readConfig(): Config {
+    const configFilePath = getConfigFilePath();
+    const rawConfig = fs.readFileSync(configFilePath, "utf-8");
+    const parsedConfig = JSON.parse(rawConfig);
+    return validateConfig(parsedConfig);
+}
+
+export function setUser(userName: string) {
+    const config = readConfig();
+    config.currentUserName = userName;
+    writeConfig(config);
+}
+
+export type Config = {
+    dbUrl: string;
+    currentUserName: string;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
+import { readConfig, setUser } from "./config";
+
 function main() {
-    console.log("Hello, world!");
+    setUser("Vladimir");
+    const config = readConfig();
+    console.log(config);
 }
 
 main();
